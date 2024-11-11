6cfa6136fd8e524fe42ef72a26174246f6482264
whznot <vonmothur@gmail.com>
2024-11-11 23:42:47 +0100

    second commit

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,15 +1,21 @@
 import styles from "../styles/PostList.module.css";
+import { IoIosArrowRoundDown } from "react-icons/io";
 
 function PostList() {
   return (
     <div className={styles["post-writing-box"]}>
-      <input
+      <textarea
         className={styles["post-input"]}
         placeholder="Drop down your thoughts..."
-        type="text"
+        rows={1}
         aria-label="Post input"
+        onInput={(e) => {
+          e.target.style.height = "auto";
+        }}
       />
-      <button></button>
+      <button>
+        <IoIosArrowRoundDown size={32} />
+      </button>
     </div>
   );
 }
